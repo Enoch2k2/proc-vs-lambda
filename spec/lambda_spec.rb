8b70141753f89b5a39ef5949f30664c16aa451f5
Enoch2k2 <enoch2k2@hotmail.com>
2017-02-10 11:22:23 -0600

    fixed lambda add spec

--- spec/lambda_spec.rb
+++ spec/lambda_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/lambda.rb'
 
 RSpec.describe 'Lambda' do
-  describe '#lambda' do
+  describe '#add' do
     it 'returns a lambda object that adds 2 to a number passed in' do
       result = add
       expect(add.call(2)).to eq(4)
