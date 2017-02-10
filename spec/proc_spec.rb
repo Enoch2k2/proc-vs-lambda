f8077348b743da1f463829c95c362d874667cb69
Enoch2k2 <enoch2k2@hotmail.com>
2017-02-10 11:09:44 -0600

    updated proc test to specify return

--- spec/proc_spec.rb
+++ spec/proc_spec.rb
@@ -2,7 +2,7 @@ require_relative '../lib/proc.rb'
 
 RSpec.describe 'Proc' do
   describe "#say" do
-    it 'creates a proc object that accepts a string when called that returns "you entered + string"' do
+    it 'returns a proc object that accepts a string when called that returns "you entered + string"' do
       expect(say.call("Hello World!")).to eq("you entered Hello World!")
     end
   end
